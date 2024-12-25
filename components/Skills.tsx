a7e72f36c7602232b5c17e1ad54c40a899c0ce9c
Himanshu Kashyap <himanshu27kashyap@gmail.com>
2024-12-25 10:36:54 +0530

    chore: Add backend and database skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,6 +10,10 @@ export const Skills: NextPage<ISKillsProps> = ({ skills }) => {
   const languages = skills?.filter(skill => skill?.fieldType?.toLowerCase() === SKILLSET.LANGUAGES);
   const frontend = skills?.filter(skill => skill?.fieldType?.toLowerCase() === SKILLSET.FRONTEND);
   const uilibraries = skills?.filter(skill => skill?.fieldType?.toLowerCase() === SKILLSET.UI_LIBRARIES);
+  const backend = skills?.filter(
+    skill =>
+      skill?.fieldType?.toLowerCase() === SKILLSET.BACKEND || skill?.fieldType?.toLowerCase() === SKILLSET.DATABASE
+  );
   const headlessCms = skills?.filter(skill => skill?.fieldType?.toLowerCase() === SKILLSET.HEADLESS_CMS);
   const testing_tools = skills?.filter(
     skill => skill?.fieldType?.toLowerCase() === SKILLSET.TESTING || skill?.fieldType?.toLowerCase() === SKILLSET.TOOLS
@@ -23,6 +27,7 @@ export const Skills: NextPage<ISKillsProps> = ({ skills }) => {
         <Skill skills={languages} skill="Languages" />
         <Skill skills={frontend} skill="Frontend" />
         <Skill skills={uilibraries} skill="UI Libraries" />
+        <Skill skills={backend} skill="Backend & Database" />
         <Skill skills={headlessCms} skill="Headless CMS" />
         <Skill skills={testing_tools} skill="Testing &amp; Other Tools" />
         <Skill skills={familiar} skill="Familiar" />
